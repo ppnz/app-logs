a915cd883f345edf24ff0fbd7a4114c6e0c95b3e
Pichavarit Panyokaew <pichavarit_p@tripetch-it.co.th>
2024-08-28 15:13:23 +0700

    add test error

--- apps/frontend/app/api/test/route.ts
+++ apps/frontend/app/api/test/route.ts
@@ -2,6 +2,6 @@ export const dynamic = 'force-static'
  
 export async function GET() {
   for (let i = 0; i < 4000000000; i++) {}
-  console.log('hello')
+  console.error('hello')
   return Response.json({ hello: 'world' })
 }
--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -4,6 +4,9 @@
 export default function Home() {
   const onSubmit = async (code: string) => {
     try {
+      if (code === 'test') {
+        return await fetch('/api/test').then(res => res.json())
+      }
       const data = { code }
       const response = await fetch('/api/logs', {
         method: 'post', 
@@ -26,6 +29,7 @@ export default function Home() {
       <p>Click the button to fetch and return the HTTP status code corresponding to the button text</p>
       <div className="flex items-center gap-4 p-4">
         <button className="px-4 py-2 rounded-sm bg-blue-500" onClick={() => onSubmit('200')}>200</button>
+        <button className="px-4 py-2 rounded-sm bg-blue-500" onClick={() => onSubmit('test')}>test</button>
         <button className="px-4 py-2 bg-red-500" onClick={() => onSubmit('400')}>400</button>
         <button className="px-4 py-2 bg-orange-500" onClick={() => onSubmit('500')}>500</button>
       </div>
