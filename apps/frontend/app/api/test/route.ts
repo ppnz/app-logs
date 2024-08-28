28a0cdd8f2210dd2893e0ca31c87f2c37df0d524
Pichavarit Panyokaew <pichavarit_p@tripetch-it.co.th>
2024-08-28 15:07:43 +0700

    add console

--- apps/frontend/app/api/test/route.ts
+++ apps/frontend/app/api/test/route.ts
@@ -2,5 +2,6 @@ export const dynamic = 'force-static'
  
 export async function GET() {
   for (let i = 0; i < 4000000000; i++) {}
+  console.log('hello')
   return Response.json({ hello: 'world' })
 }
